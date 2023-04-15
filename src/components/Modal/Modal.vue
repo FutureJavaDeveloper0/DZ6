4bba1644646b98885e510f3822b76a3384670619
Denial <den4ic369@gmail.com>
2023-04-15 19:38:24 +0300

    first

---
+++ src/components/Modal/Modal.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { Teleport,Transition } from "vue";
+import css from "./Modal.module.css";
+
+defineEmits(["close"]);
+defineProps(["show"]);
+
+</script>
+
+<template>
+    <Teleport to="#modal">
+        <Transition>
+            <div :class="css.modal__overlay" v-if="show" @click.self="$emit('close')">
+                <div :class="css.modal">
+                    <div :class="css.modal__header">
+                        <button @click="$emit('close')">&times;</button>
+                    </div>
+                    <div :class="css.modal__body"><slot></slot></div>
+                </div>
+            </div>
+        </transition>
+    </Teleport>
+</template>
+
+<style scoped>
+.v-enter-active, 
+.v-leave-active{
+    transition: opacity 500ms ease;
+}
+
+.v-enter-from,
+.v-leave-to{
+    opacity: 0;
+}
+</style>
